9721d9894c4ff6e5101268637882d9f88eecfcf1
Adnan Rahmanpoor <adnanrahmanpoor@gmail.com>
2026-07-11 16:30:48 +0400

    modified config.py to include jaeger env var

--- app/config.py
+++ app/config.py
@@ -12,6 +12,7 @@ class Settings(BaseSettings):
 
     TELEGRAM_BOT_TOKEN: Optional[str] = None
     TELEGRAM_CHAT_ID: Optional[str] = None
+    OTEL_ENDPOINT: Optional[str] = None
 
     # pydantic config to load from .env
     model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
